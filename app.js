0f0b7c7978f44e9115afe420636584ee4b574198
Alex Kusov <kysov2006@bk.ru>
2021-12-19 08:57:18 +0300

    add 3d text

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 import { OrbitControls } from 'https://cdn.jsdelivr.net/npm/three@0.121.1/examples/jsm/controls/OrbitControls.js'
 import { GLTFLoader } from './three.js-master/examples/jsm/loaders/GLTFLoader.js'
-// import { FontLoader } from './three.js-master/src/loaders/FontLoader.js'
-// import { TextGeometry } from './three.js-master/src/geometries/TextGeometry.js'
+import { FontLoader } from './three.js-master/src/loaders/FontLoader.js'
+import { TextGeometry } from './three.js-master/src/geometries/TextGeometry.js'
 
 let scene, camera, renderer;
 
@@ -86,6 +86,44 @@ function init(texture) {
 
 
 
+    //3D text
+    const loaderFont = new THREE.FontLoader();
+    let geometry;
+    loaderFont.load( 'fonts/helvetiker_bold.typeface.json', 
+        //Create 3D text after loading the font
+        function ( font ) {
+            geometry = new THREE.TextGeometry( '11111111111111111', {
+                font: font,
+                size: 80,
+                height: 5,
+                curveSegments: 12,
+                bevelEnabled: true,
+                bevelThickness: 10,
+                bevelSize: 8,
+                bevelSegments: 5
+            } );
+            //Create normal vector material
+            var meshMaterial = new THREE.MeshNormalMaterial({
+                flatShading: THREE.FlatShading,
+                transparent: true,
+                opacity: 0.9
+        });
+            var mesh = new THREE.Mesh(geometry, meshMaterial);
+            mesh.position.set(-300, 0, 0);
+            scene.add(mesh);
+        },
+        //Load progress
+        function ( xhr ) {
+            console.log( (xhr.loaded / xhr.total * 100) + '% loaded' );
+        },
+        //An error occurred
+        function (err) {
+            console.log(err);
+        }
+     );
+
+
+
     //Resize
     window.addEventListener('resize', onWindowResize, false)
     
@@ -99,37 +137,37 @@ function init(texture) {
 
 
 
-    // крутим вертим
-function onPointerDown(event) {
-    event.preventDefault();
-  
-    onPointerDownPointerX = event.clientX;
-    onPointerDownPointerY = event.clientY;
-  
-    onPointerDownLon = lon;
-    onPointerDownLat = lat;
-  
-    document.addEventListener("pointermove", onPointerMove);
-    document.addEventListener("pointerup", onPointerUp);
-  }
-  
-  function onPointerMove(event) {
-    lon = (event.clientX - onPointerDownPointerX) * 0.1 + onPointerDownLon;
-    lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat;
-  }
-  
-  function onPointerUp() {
-    document.removeEventListener("pointermove", onPointerMove);
-    document.removeEventListener("pointerup", onPointerUp);
-  }
-  
-  function onDocumentMouseWheel(event) {
-    const fov = camera.fov + event.deltaY * 0.05;
-  
-    camera.fov = THREE.MathUtils.clamp(fov, 10, 75);
-  
-    camera.updateProjectionMatrix();
-  }
+    // keyboard mouse
+    function onPointerDown(event) {
+        event.preventDefault();
+      
+        onPointerDownPointerX = event.clientX;
+        onPointerDownPointerY = event.clientY;
+      
+        onPointerDownLon = lon;
+        onPointerDownLat = lat;
+      
+        document.addEventListener("pointermove", onPointerMove);
+        document.addEventListener("pointerup", onPointerUp);
+      }
+      
+      function onPointerMove(event) {
+        lon = (event.clientX - onPointerDownPointerX) * 0.1 + onPointerDownLon;
+        lat = (event.clientY - onPointerDownPointerY) * 0.1 + onPointerDownLat;
+      }
+      
+      function onPointerUp() {
+        document.removeEventListener("pointermove", onPointerMove);
+        document.removeEventListener("pointerup", onPointerUp);
+      }
+      
+      function onDocumentMouseWheel(event) {
+        const fov = camera.fov + event.deltaY * 0.05;
+      
+        camera.fov = THREE.MathUtils.clamp(fov, 10, 75);
+      
+        camera.updateProjectionMatrix();
+      }
 
 
 
@@ -143,28 +181,28 @@ function onPointerDown(event) {
 }
 
 function render() {
-    const time = Date.now();
-  
-    lon += 0.15;
-  
-    lat = Math.max(-85, Math.min(85, lat));
-    phi = THREE.MathUtils.degToRad(90 - lat);
-    theta = THREE.MathUtils.degToRad(lon);
-  
-    scene.position.x = Math.cos(time * 0.001) * 1;
-    scene.position.y = Math.sin(time * 0.001) * 1;
-    scene.position.z = Math.sin(time * 0.001) * 1;
-  
-    scene.rotation.x += 0.002;
-    scene.rotation.y += 0.003;
-  
-    camera.position.x = 1000 * Math.sin(phi) * Math.cos(theta);
-    camera.position.y = 1000 * Math.cos(phi);
-    camera.position.z = 1000 * Math.sin(phi) * Math.sin(theta);
-  
-    camera.lookAt(scene.position);
-  
+  const time = Date.now();
 
-  
-    renderer.render(scene, camera);
-  }
+  lon += 0.15;
+
+  lat = Math.max(-85, Math.min(85, lat));
+  phi = THREE.MathUtils.degToRad(90 - lat);
+  theta = THREE.MathUtils.degToRad(lon);
+
+  scene.position.x = Math.cos(time * 0.001) * 1;
+  scene.position.y = Math.sin(time * 0.001) * 1;
+  scene.position.z = Math.sin(time * 0.001) * 1;
+
+  scene.rotation.x += 0.002;
+  scene.rotation.y += 0.003;
+
+  camera.position.x = 2000 * Math.sin(phi) * Math.cos(theta);
+  camera.position.y = 1000 * Math.cos(phi);
+  camera.position.z = 1000 * Math.sin(phi) * Math.sin(theta);
+
+  camera.lookAt(scene.position);
+
+
+
+  renderer.render(scene, camera);
+}
